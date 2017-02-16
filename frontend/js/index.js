821b89ef63966af5352db9625dde80fd617186a9
mimmofalco <mimmo.falco@gmail.com>
2017-02-16 20:32:25 +0100

    setup webpack + react

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,1 +1,18 @@
-// Front-end Starting Point
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Test extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Test is a react component test</h1>
+        <hr />
+        <hr />
+        <hr />
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<Test />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// dependencies
+const webpack = require('webpack')
+const path = require('path')
+
+/// webpack configuration
+const config = {
+  context: path.resolve(__dirname, './'),
+  entry: {
+    main: './frontend/js/'
+  },
+  output: {
+    path: path.resolve(__dirname, './public/js'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        include: path.resolve(__dirname, './frontend'),
+        use: [{
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['es2015', { modules: false }],
+              ['react']
+            ]
+          }
+        }]
+      }
+    ]
+  }
+}
+
+module.exports = config
